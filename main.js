5ff4ec06ed003c6db3bb62a5b6fee838aea10007
unknown <nuranhossain@gmail.com>
2022-09-01 20:53:11 +0600

    random-user-api

---
+++ main.js
@@ -0,0 +1,44 @@
+let randomUser = () => {
+  fetch("https://randomuser.me/api/")
+    .then((res) => res.json())
+    .then((data) => randomFriend(data.results));
+};
+
+let randomFriend = (friends) => {
+  let userInfo = document.getElementById("info");
+  let img = document.getElementById("img");
+  for (friend of friends) {
+    userInfo.innerHTML = `My Name Is ${friend.name.title} ${friend.name.last}`;
+    img.src = friend.picture.large;
+  }
+  // // photos
+  // let img = document.getElementById("img");
+  // img.src = friend.picture.large;
+
+  // Phone
+  document.getElementById("phone").addEventListener("mouseover", function () {
+    userInfo.innerHTML = `My Phone Number is <h3 class="information"> ${friend.phone}</h3> `;
+  });
+  // User Name
+  document.getElementById("name").addEventListener("mouseover", function () {
+    userInfo.innerHTML = `My Name is <h3 class="information">${friend.name.title} ${friend.name.first} ${friend.name.last}</h3>`;
+  });
+  // User Adress
+  document.getElementById("address").addEventListener("mouseover", function () {
+    userInfo.innerHTML = `My Adress is <h3 class="information"> ${
+      friend.location.street.name
+    }, ${(friend.location.city, friend.location.country)} </h3>`;
+  });
+};
+
+// Add friend list
+
+document.getElementById("add-friend").addEventListener("click", function () {
+  let ol = document.getElementById("list");
+  let li = document.createElement("li");
+  /*  */
+  li.innerHTML = `<img class="rounded-full" src=${friend.picture.thumbnail}></img> <p>${friend.name.last}</p>`;
+  ol.append(li);
+});
+
+randomUser();
